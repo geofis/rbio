44acfdb5d78e7475f0047be109c17838ad34f4be
Jose Ramon <joseramon@geografiafisica.org>
2018-08-01 00:24:36 -0400

    coldiss with ggplot2

--- coldissgg.R
+++ coldissgg.R
@@ -15,31 +15,29 @@ coldissgg <- function(dist, ordered = T) {
   gg1 <- ggplot(dist.g, aes(Var1, Var2)) +
     geom_tile(aes(fill=value), colour = "white") +
     scale_fill_gradientn(colours = mypalette(100), na.value = 'white') +
-    # scale_fill_gradient(low = "darkgreen", high = "white", na.value = 'white') +
     geom_text(aes(label=round(value,2))) +
     labs(title='Dissimilarity matrix') +
     theme(
-      text = element_text(size = 14),
+      text = element_text(size = 16),
       axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
       axis.title.x=element_blank(),
       axis.title.y=element_blank(),
       legend.position = 'none',
-      plot.title = element_text(size=16, hjust = 0.5)
+      plot.title = element_text(size=18, hjust = 0.5)
       ) +
     coord_equal()
   gg2 <- ggplot(dist.g.o, aes(Var1, Var2)) +
     geom_tile(aes(fill=value), colour = "white") +
     scale_fill_gradientn(colours = mypalette(100), na.value = 'white') +
-    # scale_fill_gradient(low = "darkgreen", high = "white", na.value = 'white') +
     geom_text(aes(label=round(value,2))) +
     labs(title='Ordered dissimilarity matrix') +
     theme(
-      text = element_text(size = 14),
+      text = element_text(size = 16),
       axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
       axis.title.x=element_blank(),
       axis.title.y=element_blank(),
       legend.position = 'none',
-      plot.title = element_text(size=16, hjust = 0.5)
+      plot.title = element_text(size=18, hjust = 0.5)
     ) +
     coord_equal()
   if(ordered) print(gg2) else print(gg1)
