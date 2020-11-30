2c5be503c7edc6a8f129a881db7f9cbd4cba8911
Jose Ramon <joseramon@geografiafisica.org>
2020-11-30 09:17:30 -0400

    Changed method argument of diversityresult, adapting to new BiodiversityR package

--- indices.R
+++ indices.R
@@ -3,7 +3,28 @@ dind <- function(mc, nomsitios=NA){
   require(vegan)
   require(BiodiversityR)
   require(fossil)
-  zz1 <- cbind(ID=rownames(mc),as.data.frame(sapply(c("richness","abundance","Shannon","Simpson","inverseSimpson","Logalpha","Berger","Jevenness","Eevenness" ),function(x) diversityresult(mc,index=x,method='s', digits = 4))))
+  zz1 <- cbind(
+    ID=rownames(mc),
+    as.data.frame(
+      sapply(
+        c("richness",
+          "abundance",
+          "Shannon",
+          "Simpson",
+          "inverseSimpson",
+          "Logalpha",
+          "Berger",
+          "Jevenness",
+          "Eevenness" ),
+        function(x)
+          if(
+            package_version(packageVersion("BiodiversityR")) >
+            package_version("2.9-1"))
+            diversityresult(mc,index=x,method='each site', digits = 4) else
+              diversityresult(mc,index=x,method='s', digits = 4)
+      )
+    )
+  )
   chao <- data.frame(ID=rownames(mc), `estimador Chao1`=sapply(rownames(mc), function(x) chao1(mc[x,], taxa.row = F)))
   jack <- data.frame(ID=rownames(mc), `estimador Jackknife`=sapply(rownames(mc), function(x) jack1(mc[x,], taxa.row = F)))
   zz2 <- Reduce(function(x, y) merge(x, y, all=TRUE), list(zz1, chao, jack))
